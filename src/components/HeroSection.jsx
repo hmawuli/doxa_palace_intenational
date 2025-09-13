311db79d21d360a86e1ed0c90aeade6919bdc198
hormekumawuli93 <hormekumawuli93@gmail.com>
2025-09-13 05:47:57 +0000

    reworked on the home page

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+jsx
+import React from 'react';
+import { Box } from '@mui/material';
+
+function HeroSection() {
+  return (
+    <Box>
+      Hello, Hero!
+    </Box>
+  );
+}
+
+export default HeroSection;
